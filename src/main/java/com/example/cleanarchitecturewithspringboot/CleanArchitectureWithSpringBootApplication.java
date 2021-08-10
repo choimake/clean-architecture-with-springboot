ef1a4d96f2665ef9e045400216b3d4ae497e02bb
HirokiMaeyama <hmaeyama@HirokiMaeyamanoMacBook-Pro.local>
2021-08-11 07:37:52 +0900

    first commit

---
+++ src/main/java/com/example/cleanarchitecturewithspringboot/CleanArchitectureWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.cleanarchitecturewithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArchitectureWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cleanarchitecturewithspringboot/CleanArchitectureWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cleanarchitecturewithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
